5bb95f543de3026a0f95b4ff68beb4c398bc55dd
jiwoongjeon <brady7340@naver.com>
2022-04-10 12:46:41 -0400

    final

--- app/index.js
+++ app/index.js
@@ -6,6 +6,71 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const readSql =
+    'select ga, va, pa, ma, li, digital, drawing, painting, sculpture, cinema, theater, dance, music, architecture, history, '
+    + 'p_type.*, p_content.* '
+    + 'from user '
+    + 'join p_general_info on user.p_general_id = p_general_info.id '
+    + 'join p_category on user.p_category_id = p_category.id '
+    + 'join p_type on user.p_type_id = p_type.id '
+    + 'join p_content on user.p_content_id = p_content.id '
+
+const courseSql =
+    'select course.name, user_id, course_type.*, course_content.* '
+    + 'from taken_courses '
+    + 'join course on course_id = course.id '
+    + 'join course_type on course.type_id = course_type.id '
+    + 'join course_content on course.content_id = course_content.id '
+    + 'where user_id = ? or user_id = ? or user_id = ? or user_id=? or user_id=? or user_id=? or user_id=? or user_id=? or user_id=? '
+
+app.post('/server', (req, res) => {
+    // let user_list = Object.keys(req.body).sort().reduce((obj, key) => (obj[key] = req.body[key], obj), {})
+    // console.log(Object.values(user_list))
+    userData = Object.values(req.body)
+    // console.log(userData)
+    let simlist = {}
+    config.db.query(readSql, (err, result) => {
+        if (err) console.log(err);
+
+        for (i in result) {
+            let total = 0
+            let comm = 0
+            temp = Object.keys(result[i]).sort().reduce((obj, key) => (obj[key] = result[i][key], obj), {})
+            temp = Object.values(temp)
+            for (let j = 0; j<45; j++) {
+                if (j==27) continue;
+                if (j>10 & j<26) {
+                    continue;
+                }
+                if (userData[j] != temp[j]) {
+                    total += 1;
+                } else if (userData[j] == temp[j] & userData == null) {
+                    continue;
+                } else {
+                    comm += 1;
+                    total += 1;
+                }
+            }
+            simlist[((comm/total)*100).toFixed(2)] = parseInt(i, 10)+12
+            console.log(total, comm, ((comm/total)*100).toFixed(2))
+        }
+        console.log(simlist)
+        // retjson = parseFloat(Object.keys(simlist))
+        let retjson = Object.keys(simlist).sort((a,b) => b-a).reduce((obj, key) => (obj[key] = simlist[key], obj), {})
+        console.log(retjson)
+        arr = Object.values(retjson)
+        console.log(arr)
+        retjson = []
+        config.db.query(courseSql, arr, (error, resultb) => {
+            if (error) console.log(error);
+            console.log(resultb)
+            retjson.push(resultb);
+        })
+        console.log(retjson)
+        res.send(retjson)
+    })
+})
+
 const server = app.listen(config.express.port, () => {
     console.log(`Server running on Port ${config.express.port}`);
 });
--- app/xmltosql.js
+++ app/xmltosql.js
@@ -1,9 +1,42 @@
 const fs = require('fs');
 const path = require('path');
 const csv = require('csv-parser');
+const config = require('./config');
 
 const inputFile = path.resolve(__dirname, "./smallcoursesample.csv");
 let results = []
+
+const writeCourseSql = 
+    "insert into course (name, general_id, category_id, type_id, content_id) values (?, ?, ?, ?, ?)"
+
+const writeTypeSql = 
+    "insert into course_general_info (ga, gs, gh, gn, il) values (?)"
+
+const writeCategorySql1 =
+    "insert into course_category (main1, sub1) values (?)"
+const writeCategorySql2 = 
+    "insert into course_category (main1, sub1, main2, sub2) values (?)"
+
+const writeContent1 =
+    "insert into course_content (hash1) values (?)"
+const writeContent2 =
+    "insert into course_content (hash1, hash2) values (?)"
+const writeContent3 =
+    "insert into course_content (hash1, hash2, hash3) values (?)"
+const writeContent4 =
+    "insert into course_content (hash1, hash2, hash3, hash4) values (?)"
+const writeContent5 =
+    "insert into course_content (hash1, hash2, hash3, hash4, hash5) values (?)"
+const writeContent6 =
+    "insert into course_content (hash1, hash2, hash3, hash4, hash5, hash6) values (?)"
+const writeContent7 =
+    "insert into course_content (hash1, hash2, hash3, hash4, hash5, hash6, hash7) values (?)"
+const writeContent8 =
+    "insert into course_content (hash1, hash2, hash3, hash4, hash5, hash6, hash7, hash8) values (?)"
+const writeContent9 =
+    "insert into course_content (hash1, hash2, hash3, hash4, hash5, hash6, hash7, hash8, hash9) values (?)"
+const writeContent10 =
+    "insert into course_content (hash1, hash2, hash3, hash4, hash5, hash6, hash7, hash8, hash9, hash10) values (?)"
 
 fs.createReadStream(inputFile)
   .pipe(csv())
@@ -24,12 +57,16 @@ fs.createReadStream(inputFile)
 
     let temp = [];
 
-    if (ga) temp.push('GA')
-    if (gn) temp.push('GN')
-    if (gs) temp.push('GS')
-    if (gh) temp.push('GH')
-    if (il) temp.push('IL')
-
+    if (ga) temp.push(1)
+    else temp.push(0)
+    if (gs) temp.push(1)
+    else temp.push(0)
+    if (gh) temp.push(1)
+    else temp.push(0)
+    if (gn) temp.push(1)
+    else temp.push(0)
+    if (il) temp.push(1)
+    else temp.push(0)
 
     // console.log(temp)
 
@@ -40,16 +77,117 @@ fs.createReadStream(inputFile)
     // results.push(row);
   })
   .on('end', () => {
-    console.log(results);
+    // console.log(results);
+    for (row in results) {
+        // Course name
+        console.log(results[row][0]);
+        console.log(parseInt(row, 10)+1)
+        config.db.query(writeCourseSql, [results[row][0], parseInt(row, 10)+1, parseInt(row, 10)+1, parseInt(row, 10)+1, parseInt(row, 10)+1], (err, result) => {
+            if (err) console.log(err)
+        })
+
+        // Course general info
+
+        // console.log(results[row][1])
+        // config.db.query(writeTypeSql, [results[row][1]], (err, result) => {
+        //     if (err) console.log(err);
+        // })
+
+        // Course category
+        // let temp = []
+        // console.log(results[row][2])
+        // console.log(results[row][3])
+        // if (results[row][2].includes('Visual Art')) {
+        //     temp.push(0)
+        //     if (results[row][3].includes('Digital') | results[row][3].includes('digital')){
+        //         temp.push(0)
+        //     } else if (results[row][3].includes('Drawing') | results[row][3].includes('drawing')){
+        //         temp.push(1)
+        //     } else if (results[row][3].includes('Painting') | results[row][3].includes('painting')){
+        //         temp.push(2)
+        //     } else if (results[row][3].includes('Sculpture') | results[row][3].includes('sculpture')){
+        //         temp.push(3);
+        //     } else {
+        //         temp.push(4);
+        //     }
+        // }
+        // if (results[row][2].includes('Media Art')) {
+        //     temp.push(2)
+        //     if (results[row][3].includes('Digital') | results[row][3].includes('digital')){
+        //         temp.push(0)
+        //     } else {
+        //         temp.push(1)
+        //     }
+        // }
+        // if (results[row][2].includes('Literature')) {
+        //     temp.push(3)
+        //     if (results[row][3].includes('Architecture') | results[row][3].includes('architecture')){
+        //         temp.push(0)
+        //     } else {
+        //         temp.push(1)
+        //     }
+        // }
+        // if (temp.length == 2) {
+        //     config.db.query(writeCategorySql1, [temp], (err, result) => {
+        //         if (err) console.log(err)
+        //     })
+        // }
+        // else {
+        //     config.db.query(writeCategorySql2, [temp], (err, result) => {
+        //         if (err) console.log(err)
+        //     })
+        // }
+
+        // Course Contents
+ 
+        // if (results[row][5].substr(results[row][5].length-1, 1) == ",") {
+        //     results[row][5] = results[row][5].slice(0, results[row][5].length-1)
+        //     console.log(results[row][5])
+        // }
+        // if (results[row][5].split(",").length == 1) {
+        //     config.db.query(writeContent1, [results[row][5].split(",")], (err, result) => {
+        //         if (err) console.log(err);
+        //     })
+        // } else if (results[row][5].split(",").length == 2){
+        //     config.db.query(writeContent2, [results[row][5].split(",")], (err, result) => {
+        //         if (err) console.log(err);
+        //     })
+        // } else if (results[row][5].split(",").length == 3){
+        //     config.db.query(writeContent3, [results[row][5].split(",")], (err, result) => {
+        //         if (err) console.log(err);
+        //     })
+        // } else if (results[row][5].split(",").length == 4){
+        //     config.db.query(writeContent4, [results[row][5].split(",")], (err, result) => {
+        //         if (err) console.log(err);
+        //     })
+        // } else if (results[row][5].split(",").length == 5){
+        //     config.db.query(writeContent5, [results[row][5].split(",")], (err, result) => {
+        //         if (err) console.log(err);
+        //     })
+        // } else if (results[row][5].split(",").length == 6){
+        //     config.db.query(writeContent6, [results[row][5].split(",")], (err, result) => {
+        //         if (err) console.log(err);
+        //     })
+        // } else if (results[row][5].split(",").length == 7){
+        //     config.db.query(writeContent7, [results[row][5].split(",")], (err, result) => {
+        //         if (err) console.log(err);
+        //     })
+        // } else if (results[row][5].split(",").length == 8){
+        //     config.db.query(writeContent8, [results[row][5].split(",")], (err, result) => {
+        //         if (err) console.log(err);
+        //     })
+        // } else if (results[row][5].split(",").length == 9){
+        //     config.db.query(writeContent9, [results[row][5].split(",")], (err, result) => {
+        //         if (err) console.log(err);
+        //     })
+        // } else if (results[row][5].split(",").length == 10){
+        //     config.db.query(writeContent10, [results[row][5].split(",")], (err, result) => {
+        //         if (err) console.log(err);
+        //     })
+        // } 
+
+
+        // console.log(results[row][5])
+
+    }
   });
-
-// const reader = require('xlsx');
-
-// let data = [];
-
-// const file = reader.readFile('./test.xlsx');
-
-// const temp = reader.utils.sheet_to_json(file.Sheets[file.SheetNames[0]]);
-// temp.forEach((res) => {
-//   data.push(res);
-// })
